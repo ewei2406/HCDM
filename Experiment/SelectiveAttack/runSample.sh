7ea88f3964a0158ea1f1a3d5a9974ce0a030c7b4
Edward Wei <ewei2406@gmail.com>
2022-04-05 20:12:32 -0400

    results

---
+++ Experiment/SelectiveAttack/runSample.sh
@@ -0,0 +1,15 @@
+datasets=(cora citeseer Polblogs flickr BlogCatalog)
+
+
+for dataset in "${datasets[@]}"
+do
+    for seed in {100..104}
+    do
+        for ptb_rate in 0.1 0.25 0.5
+        do
+            python3 SelectiveAttack.py --ptb_rate $ptb_rate --dataset $dataset --seed $seed --do_sampling Y --save Y --save_location "./NoSampleResults.csv" --check_universal Y
+        done
+    done
+done
+
+seeds=()
